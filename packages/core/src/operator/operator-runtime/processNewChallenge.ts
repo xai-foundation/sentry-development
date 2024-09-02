68edb26912324d685bc482ba0065a7ca8cac4606
CryptIT Austria <office@cryptit.at>
2024-09-02 19:22:39 +0200

    remove demo boostfacktor for bulk submission

--- packages/core/src/operator/operator-runtime/processNewChallenge.ts
+++ packages/core/src/operator/operator-runtime/processNewChallenge.ts
@@ -47,7 +47,7 @@ export async function processNewChallenge(
 
             const winningKeyCount = getWinningKeyCount(
                 ownerOrPool.keyCount,
-                Number(boostFactor) * 100,
+                Number(boostFactor),
                 ownerOrPool.address,
                 challengeId,
                 challenge.assertionStateRootOrConfirmData,
