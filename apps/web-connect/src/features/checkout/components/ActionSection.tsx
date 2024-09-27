906d5aad0d63f92aef122093c57f81cf04a1556d
Jerry Hodges <jerryhodges@expopulus.com>
2024-09-27 19:02:55 -0400

    Added checks to disable buttons if wallet is not connected.

--- apps/web-connect/src/features/checkout/components/ActionSection.tsx
+++ apps/web-connect/src/features/checkout/components/ActionSection.tsx
@@ -67,7 +67,7 @@ export function ActionSection(): JSX.Element {
                     <PrimaryButton
                         onClick={() => handleMintWithEthClicked()}
                         className={`w-full h-16 ${ready ? "bg-[#F30919] global-clip-path" : "bg-gray-400 cursor-default !text-[#726F6F]"} text-lg text-white p-2 uppercase font-bold`}
-                        isDisabled={!ready || getEthButtonText().startsWith("Insufficient")}
+                        isDisabled={!ready || getEthButtonText().startsWith("Insufficient") || !isConnected}
                         btnText={getEthButtonText()}
                     />
                     <br />
@@ -81,7 +81,7 @@ export function ActionSection(): JSX.Element {
                     <PrimaryButton
                         onClick={handleBuyWithXaiClicked}
                         className={`w-full h-16 ${ready ? "bg-[#F30919] global-clip-path" : "bg-gray-400 cursor-default !text-[#726F6F]"} text-lg text-white p-2 uppercase font-bold`}
-                        isDisabled={!ready || !userHasTokenBalance}
+                        isDisabled={!ready || !userHasTokenBalance || !isConnected}
                         btnText={getTokenButtonText()}
                     />
                 )}
